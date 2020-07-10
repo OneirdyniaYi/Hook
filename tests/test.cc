834a84fa02a15674f698bd0f9578492e07e507e1
OneirdyniaYi <1057711801@qq.com>
2020-07-10 10:49:37 +0800

    20200710_4

--- tests/test.cc
+++ tests/test.cc
@@ -4,7 +4,7 @@
 int main(int argc,char** ragv){
 	hook::Logger::ptr logger(new hook::Logger);
 	logger->addAppender(hook::LogAppender::ptr(new hook::StdoutLogAppender));
-	hook::LogEvent::ptr event(new hook::LogEvent(logger,LogLevel::Level::DEBUG,__FILE__,__LINE__,0,1,2,time(0)));
+	hook::LogEvent::ptr event(new hook::LogEvent(logger,hook::LogLevel::Level::DEBUG,__FILE__,__LINE__,0,1,2,time(0)));
 
 	logger->log(hook::LogLevel::DEBUG,event);
 
